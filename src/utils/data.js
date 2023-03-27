880945fac4a25a31604537a302d133eea73bd623
Niraj Pandey <pandeyn2@nku.edu>
2023-03-26 22:38:02 -0400

    first commit

---
+++ src/utils/data.js
@@ -0,0 +1,16 @@
+export const data = [
+    
+    {
+        "author": "Kaya Olsen",
+        "title": "How to Master Your Disruption Mindset and Bring Change to Our Society",
+        "description": "https://moonshotpirates.com/blog/how-to-master-your-disruption-mindset-and-bring-change-to-our-society/",
+        "url": "https://www.moonshotpirates.com/blog/",
+        
+        
+        "category": "general",
+        "language": "en",
+        "country": "us",
+        "published_at": "2020-08-01T05:34:47+00:00"
+    },
+    
+]
