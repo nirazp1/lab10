8997db833a38b9bcfd4e8ea823989f3f0feab8ce
Niraj Pandey <pandeyn2@nku.edu>
2023-03-31 23:08:53 -0400

    Add untracked files

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,16 +1,18 @@
 export const data = [
     
-    {
-        "author": "Kaya Olsen",
-        "title": "How to Master Your Disruption Mindset and Bring Change to Our Society",
-        "description": "https://moonshotpirates.com/blog/how-to-master-your-disruption-mindset-and-bring-change-to-our-society/",
-        "url": "https://www.moonshotpirates.com/blog/",
+        {
+            "author": "John Smith",
+            "title": "John Smith (explorer)\b\n\n\n",
+            "description": "John Smith (baptized 6 January 1580 – 21 June 1631) was an English soldier, explorer, colonial governor, admiral of New England, and author. He played an important role in the establishment of the colony at Jamestown, Virginia, the first permanent English settlement in North America, in the early 17th century. He was a leader of the Virginia Colony between September 1608 and August 1609, and he led an exploration along the rivers of Virginia and the Chesapeake Bay, during which he became the first English explorer to map the Chesapeake Bay area. Later, he explored and mapped the coast of New England. He was knighted for his services to Sigismund Báthory, Prince of Transylvania, and his friend Mózes Székely.",
+            "url": "https://en.wikipedia.org/wiki/John_Smith_%28explorer%29",
+            "source": "Example News",
+            "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Houghton_STC_22790_-_Generall_Historie_of_Virginia%2C_New_England%2C_and_the_Summer_Isles%2C_John_Smith.jpg/330px-Houghton_STC_22790_-_Generall_Historie_of_Virginia%2C_New_England%2C_and_the_Summer_Isles%2C_John_Smith.jpg",
+            
+            "language": "en",
+            "country": "us",
+            "published_at": "2022-03-30T10:00:00+00:00"
+        },
         
         
-        "category": "general",
-        "language": "en",
-        "country": "us",
-        "published_at": "2020-08-01T05:34:47+00:00"
-    },
-    
+            
 ]
