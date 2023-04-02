0d8200848c58b6d05dfcd03a1108faf4759edf9b
Niraj Pandey <115514446+nirazp1@users.noreply.github.com>
2023-04-01 21:18:12 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import './App.css';
-import Container from './components/Container';
-import Navbar from './components/Navbar';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const apiKey = "e476cbd0b7324f68b7972cbda630aab0";
+    const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
+
+    axios.get(apiUrl)
+      .then(response => {
+        setArticles(response.data.articles);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
   return (
-    <>
-      <Navbar />
-      <div>
-        <Container />
-      </div>
-    </>
+    <div>
+      <h1>Top Headlines</h1>
+      <ul>
+        {articles.map(article => (
+          <li key={article.url}>
+            <a href={article.url} target="_blank" rel="noreferrer">{article.title}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
