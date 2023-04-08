849ddf0a5729d699a8122a908d800fca3b20ea10
Niraj Pandey <pandeyn2@nku.edu>
2023-04-08 00:47:31 -0400

    lab12 by Niraj and Smaran

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import request from 'browser-request';
-import { useState } from 'react';
+import React, { useState, useEffect } from "react";
+import request from "browser-request";
 
-function useNewsAPI() {
+function App() {
   const [articles, setArticles] = useState([]);
 
-  const fetchArticles = () => {
-    request.get(
-      'https://newsapi.org/v2/top-headlines?country=us&apiKey=YOUR_API_KEY',
-      (err, res, body) => {
-        if (err) {
-          console.error(err);
-        } else {
-          const data = JSON.parse(body);
-          setArticles(data.articles);
-        }
-      }
-    );
-  };
+  useEffect(() => {
+    const apiKey = "e476cbd0b7324f68b7972cbda630aab0";
+    const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
 
-  return [articles, fetchArticles];
+    request(apiUrl, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        const data = JSON.parse(body);
+        setArticles(data.articles);
+      } else {
+        console.log(error);
+      }
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>Top Headlines</h1>
+      <ul>
+        {articles.map(article => (
+          <li key={article.url}>
+            <a href={article.url} target="_blank" rel="noreferrer">{article.title}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
-;
+
+export default App;
